f0086aa06098d61560ac99267a59d850f5956a6b
Claudio Júnior <claudiojunior9662@gmail.com>
2022-09-14 22:19:01 -0300

    [ADD] aula 18 e 19

--- appMinhaIdeia/appminhaideia/src/main/java/curso/android/appminhaideia/SplashActivity.java
+++ appMinhaIdeia/appminhaideia/src/main/java/curso/android/appminhaideia/SplashActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
     String TAG = "APP_MINHA_IDEIA";
+
+    int tempoDeEspera = 1000 * 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +23,16 @@ public class SplashActivity extends AppCompatActivity {
 
     private void trocarTela() {
         Log.d(TAG, "trocarTela: Mudando de tela...");
-        Intent trocarDeTela = new Intent(this, MainActivity.class);
-        startActivity(trocarDeTela);
-        finish();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "trocarTela: Trocando de tela...");
+
+                Intent trocarDeTela = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(trocarDeTela);
+                finish();
+            }
+        }, tempoDeEspera);
     }
 }
