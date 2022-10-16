2a1eaeafc9fdc60fc8d4f5fdbbd4e2c180437c23
Claudio Júnior <claudiojunior9662@gmail.com>
2022-10-16 11:57:28 -0300

    [ADD] aula 59

--- AppMinhaIdeiaDB/app/src/main/java/curso/android/appminhaideiadb/controller/ClienteController.java
+++ AppMinhaIdeiaDB/app/src/main/java/curso/android/appminhaideiadb/controller/ClienteController.java
@@ -1,4 +1,16 @@
 package curso.android.appminhaideiadb.controller;
 
-public class ClienteController {
+import android.content.Context;
+import android.util.Log;
+
+import curso.android.appminhaideiadb.api.AppUtil;
+import curso.android.appminhaideiadb.datasource.AppDataBase;
+
+public class ClienteController extends AppDataBase {
+
+    public ClienteController(Context context) {
+        super(context);
+
+        Log.d(AppUtil.TAG, "ClienteController: conectado");
+    }
 }
--- AppMinhaIdeiaDB/app/src/main/java/curso/android/appminhaideiadb/view/MainActivity.java
+++ AppMinhaIdeiaDB/app/src/main/java/curso/android/appminhaideiadb/view/MainActivity.java
@@ -3,14 +3,23 @@ package curso.android.appminhaideiadb.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import curso.android.appminhaideiadb.R;
+import curso.android.appminhaideiadb.api.AppUtil;
+import curso.android.appminhaideiadb.controller.ClienteController;
 
 public class MainActivity extends AppCompatActivity {
+
+    ClienteController clienteController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(AppUtil.TAG, "onCreate: app minha ideia database");
+
+        clienteController = new ClienteController(getApplicationContext());
     }
 }
